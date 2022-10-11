1297c4e6cc03a9feaa971a44dd867d73875b27a4
Ernesto Corona <ernesto_rodriguezc2002@outlook.com>
2022-10-10 18:01:49 -0700

    El codigo apenas se va iniciar

---
+++ Programación/Baymax-Code-Beta/Baymax-Code-Beta.ino
@@ -0,0 +1,17 @@
+
+/*
+ * Universidad Tecnológica de Tijuana
+ * Ingeniería en Mecatrónica
+ * Robot Rescatista "Baymax"
+ * Autor: TSU. Rodríguez Corona Ernesto
+ * 7A
+ */
+void setup() {
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
