64ecb4d929ab0f55ad3b9bb1589e6b26d673ea53
Ernesto Corona <ernesto_rodriguezc2002@outlook.com>
2022-10-30 21:29:42 -0700

    ESTADO: MONITOREO Y CONTROL

--- Programación/Baymax-Code-Beta/Baymax-Code-Beta.ino
+++ Programación/Baymax-Code-Beta/Baymax-Code-Beta.ino
@@ -2,50 +2,83 @@
  * Universidad Tecnológica de Tijuana
  * Ingeniería en Mecatrónica
  * Robot Rescatista "Baymax"
- * Autor: TSU. Rodríguez Corona Ernesto
+ * TSU. Rodríguez Corona Ernesto
  * 7A
  */
-
+ 
+/*
+ * Créditos a sus respectivos autores con respecto al algoritmo PID para el control de linea negra del robot
+ * El algoritmo de búsqueda sí fue elaborado por el programador de este código
+ * 
+ * Versión 0.2 (sigue en testeo) 
+ *  
+ */
+ 
 //Libreria QTR proporcionada por el fabricante
 
 #include <QTRSensors.h>
-QTRSensors qtr;
-const uint8_t SensorCount = 8;
-uint16_t sensorValues[SensorCount];
 
 //Definicion de variables constantes en el proyecto
-#define velocidad 150
+
+#define NUM_SENSORS   8  //numero de sensores usados
+#define TIMEOUT       2500  // tiempo de espera para dar resultado en uS
+#define ir  13    //pin led on
 #define ledCalibracion 2
 #define PWMotorA 3
-#define MotorAIzquierdo 4
+#define MotorARetroceso 4
 #define MotorADerecho 5
 #define ParoMotores 6
 #define MotorBDerecho 7
-#define MotorBIzquierdo 8
+#define MotorBRetroceso 8
 #define PWMotorB 9
 #define botonCalibracion 12
 #define IR 13
+
 int tiempoCalibracion = 20;
+////Algoritmo PID proporcionado por la página https://aprendiendofacilelectronica.blogspot.com/2015/04/modificacion-libreria-qtr-sensors-para_18.html 
+
+QTRSensorsRC qtr((unsigned char[]) {A7, A6, A5, A4, A3, A2, A1, A0}, NUM_SENSORS, TIMEOUT, IR);
+
+unsigned int sensorValues[NUM_SENSORS];
+unsigned int position = 0;
+
+/// variables para el pid
+int  derivativo=0, proporcional=0, integral=0; //parametros
+int  salida_pwm=0, proporcional_pasado=0;
+
+
+//_______AQUI CAMBIEREMOS LOS PARAMETROS DE NUESTRO ROBOT_________________
+int velocidad=70; //variable para la velocidad, el maximo es 255
+float Kp=0.18;
+float Kd=2;
+float Ki=0.01;  //constantes
+//variables para el control del sensado
+int linea=0;                //  0 para lineas negra, 1 para lineas blancas
+int flanco_color =0;      // aumenta o disminuye el valor del sensado
+int en_linea=500;         //valor al que considerara si el sensor esta en linea o no
+int ruido=  50;          //valor al cual el valor del sensor es considerado como ruido
+//________________________________________________________________________________
+
+//Algoritmo PID proporcionado por la página https://aprendiendofacilelectronica.blogspot.com/2015/04/modificacion-libreria-qtr-sensors-para_18.html
+
+// variables para la comunicacion LV-Arduino
+char esperandoDato;
 
 void setup() {
   Serial.begin (9600);
   //Salidas
   pinMode (ledCalibracion, OUTPUT);
   pinMode (PWMotorA, OUTPUT);
-  pinMode (MotorAIzquierdo, OUTPUT);
+  pinMode (MotorARetroceso, OUTPUT);
   pinMode (MotorADerecho, OUTPUT);
   pinMode (ParoMotores, OUTPUT);
   pinMode (MotorBDerecho, OUTPUT);
-  pinMode (MotorBIzquierdo, OUTPUT);
+  pinMode (MotorBRetroceso, OUTPUT);
   pinMode (PWMotorB, OUTPUT);
   
   //Entradas 
   pinMode (botonCalibracion, INPUT);
  
-  //Declaracion de sensores QTR-8A
-  qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, SensorCount);
-  qtr.setEmitterPin(IR);
 
   //calibracion de QTR
   while (botonCalibracion); //Se queda en bucle en espera de accionamiento de boton para calibrar
@@ -59,52 +92,128 @@ void setup() {
   // Call calibrate() 400 times to make calibration take about 10 seconds.
   */
   
-  for (uint16_t i = 0; i < 400; i++)
+  for (uint16_t i = 0; i < 200; i++)
   {
     qtr.calibrate();
   }
   digitalWrite(ledCalibracion, LOW); // una vez terminado la calibracion el led de calibracion se apaga.
-  
-  // print the calibration minimum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  
-  Serial.println();
-  Serial.println();
-  delay(1000);
+ 
 }
 
-void motorAdelante ()
+void setMotores(int motorA, int motorB)
+{
+  
+  if ( motorA >= 0 )  //motor izquierdo
+ {
+  digitalWrite(MotorADerecho, HIGH); // con high avanza
+  digitalWrite(MotorARetroceso, LOW);
+  analogWrite(PWMotorA,255-motorA); //se controla de manera inversa para mayor control
+ }
+ else
+ {
+  digitalWrite(MotorADerecho, LOW);
+  digitalWrite(MotorARetroceso,HIGH); //con low retrocede
+  motorA = motorA*(-1); //cambio de signo
+  analogWrite(PWMotorA,motorA); 
+ }
+
+
+  if ( motorB >= 0 ) //motor derecho
+ {
+  digitalWrite(MotorBDerecho, HIGH); // con high avanza
+  digitalWrite(MotorBRetroceso, LOW); //con low retrocede
+  analogWrite(PWMotorA,255-motorB);
+ }
+ else
+ {
+  digitalWrite(MotorBDerecho, LOW);
+  digitalWrite(MotorBRetroceso,HIGH); //con low retrocede
+  motorB = motorB*(-1);
+  analogWrite(PWMotorB, motorB);
+ }
+}
+
+void recepcionDatos ()
 {
   
 }
 
-void motorRetroceder()
-{
-  
-}
-
-void giroDerecha ()
-{
-  
-}
-
-void giroIzquierda ()
-{
-  
-}
 
 void loop() {
+/*
+ * Lista de caracteres que se envian
+ * 
+ */
+  if (Serial.available () != 0)
+  {
+    esperandoDato = Serial.read ();
+    
+    if (esperandoDato == 'A')
+    {
+        pid(linea, velocidad, Kp, Ki, Kd, flanco_color, en_linea, ruido); //funcion para algoritmo pid(modificado ) (tipo_linea,velocidad,kp,ki,kd,flanco_color,en_linea,ruido)
 
+         
+      frenos_contorno(linea,700); //funcion para frenado en curvas tipo  //flanco de comparación va desde 0 hasta 1000 , esto para ver si esta en negro o blanco
+    }
+    
+  }
+}
+
+//Algoritmo PID proporcionado por la página https://aprendiendofacilelectronica.blogspot.com/2015/04/modificacion-libreria-qtr-sensors-para_18.html
+ void pid(int linea, int velocidad, float Kp, float Ki, float Kd,int flanco_color, int en_linea,int ruido)
+{
+  position = qtr.readLine(sensorValues, QTR_EMITTERS_ON, linea,flanco_color, en_linea, ruido ); //0 para linea negra, 1 para linea blanca
+  proporcional = (position) - 3500; // set point es 3500, asi obtenemos el error
+  integral=integral + proporcional_pasado; //obteniendo integral (integral += proporcional_pasado)
+  derivativo = (proporcional - proporcional_pasado); //obteniedo el derivativo
+  if (integral>1000) integral = 1000; //limitamos la integral para no causar problemas
+  if (integral<-1000) integral =- 1000;
+  salida_pwm =( proporcional * Kp ) + ( derivativo * Kd )+(integral*Ki);
+  
+  if (  salida_pwm > velocidad )  salida_pwm = velocidad; //limitamos la salida de pwm
+  if ( salida_pwm < -velocidad )  salida_pwm = -velocidad;
+  
+  if (salida_pwm < 0)
+ {
+  setMotores (velocidad + salida_pwm, velocidad);
+ }
+ if (salida_pwm > 0)
+ {
+  setMotores (velocidad, velocidad-salida_pwm);
+ }
+ proporcional_pasado = proporcional;  
+}
+
+void frenos_contorno(int tipo, int flanco_comparacion)
+{
+  
+if(tipo == 0)
+{
+  if (position <= 500) //si se salio por la parte derecha de la linea
+ {
+  setMotores(-80, 90); //debido a la inercia, el motor tendera a seguri girando por eso le damos para atras , para que frene lo mas rapido posible 
+  while(true)  
+  {
+   qtr.read(sensorValues); //lectura en bruto de sensor   
+if (sensorValues[0] > flanco_comparacion || sensorValues[1] > flanco_comparacion) 
+//asegurar que esta en linea
+   {
+    break;
+   } 
+  }
+ }
+
+ if (position >= 6500) //si se salio por la parte izquierda de la linea
+ { 
+  setMotores(90, -80);
+  while(true)
+  {
+   qtr.read(sensorValues);
+if (sensorValues[7] > flanco_comparacion || sensorValues[6] > flanco_comparacion )
+   {
+    break;
+   }  
+  }
+ }
+}
 }
--- Programación/Baymax-HMI-Project.lvproj
+++ Programación/Baymax-HMI-Project.lvproj
@@ -11,7 +11,52 @@
 		<Property Name="server.vi.propertiesEnabled" Type="Bool">true</Property>
 		<Property Name="specify.custom.address" Type="Bool">false</Property>
 		<Item Name="Pantalla HMI.vi" Type="VI" URL="../Pantalla HMI.vi"/>
-		<Item Name="Dependencies" Type="Dependencies"/>
+		<Item Name="Testeo de adquisición de imagenes.vi" Type="VI" URL="../Testeo de adquisición de imagenes.vi"/>
+		<Item Name="Dependencies" Type="Dependencies">
+			<Item Name="vi.lib" Type="Folder">
+				<Item Name="Calculate Frames per Second.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Calculate Frames per Second.vi"/>
+				<Item Name="Color to RGB.vi" Type="VI" URL="/&lt;vilib&gt;/Utility/colorconv.llb/Color to RGB.vi"/>
+				<Item Name="Image Type" Type="VI" URL="/&lt;vilib&gt;/vision/Image Controls.llb/Image Type"/>
+				<Item Name="Image Unit" Type="VI" URL="/&lt;vilib&gt;/vision/Image Controls.llb/Image Unit"/>
+				<Item Name="IMAQ Copy" Type="VI" URL="/&lt;vilib&gt;/vision/Management.llb/IMAQ Copy"/>
+				<Item Name="IMAQ Create" Type="VI" URL="/&lt;vilib&gt;/vision/Basics.llb/IMAQ Create"/>
+				<Item Name="Imaq Dispose" Type="VI" URL="/&lt;vilib&gt;/vision/Basics.llb/Imaq Dispose"/>
+				<Item Name="IMAQ Get Object Tracking Session Info" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Get Object Tracking Session Info"/>
+				<Item Name="Imaq GetImageInfo" Type="VI" URL="/&lt;vilib&gt;/vision/Basics.llb/Imaq GetImageInfo"/>
+				<Item Name="IMAQ Image.ctl" Type="VI" URL="/&lt;vilib&gt;/vision/Image Controls.llb/IMAQ Image.ctl"/>
+				<Item Name="IMAQ Mean Shift Options" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Mean Shift Options"/>
+				<Item Name="IMAQ Object Tracking Options" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Object Tracking Options"/>
+				<Item Name="IMAQ Object Tracking Session.ctl" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Object Tracking Session.ctl"/>
+				<Item Name="IMAQ Object Tracking SessionType.ctl" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Object Tracking SessionType.ctl"/>
+				<Item Name="IMAQ Overlay Multiple Lines 2" Type="VI" URL="/&lt;vilib&gt;/vision/Overlay.llb/IMAQ Overlay Multiple Lines 2"/>
+				<Item Name="IMAQ Read Object Tracking Session" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Read Object Tracking Session"/>
+				<Item Name="IMAQ Set Initial Object Tracking Position" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Set Initial Object Tracking Position"/>
+				<Item Name="IMAQ Shape Adapted Options" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Shape Adapted Options"/>
+				<Item Name="IMAQ Track Objects" Type="VI" URL="/&lt;vilib&gt;/vision/Tracking.llb/IMAQ Track Objects"/>
+				<Item Name="IMAQdx.ctl" Type="VI" URL="/&lt;vilib&gt;/userdefined/High Color/IMAQdx.ctl"/>
+				<Item Name="IVA Append VI Name to GUID.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Append VI Name to GUID.vi"/>
+				<Item Name="IVA Get Alternate Path.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Get Alternate Path.vi"/>
+				<Item Name="IVA Get Numeric.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Get Numeric.vi"/>
+				<Item Name="IVA Get Point.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Get Point.vi"/>
+				<Item Name="IVA Object Tracking - Init Function.ctl" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Object Tracking - Init Function.ctl"/>
+				<Item Name="IVA Object Tracking - Init Positions.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Object Tracking - Init Positions.vi"/>
+				<Item Name="IVA Result Manager Function.ctl" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Result Manager Function.ctl"/>
+				<Item Name="IVA Result Manager.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Result Manager.vi"/>
+				<Item Name="IVA Store Object Tracking Results.vi" Type="VI" URL="/&lt;vilib&gt;/vision/Vision Assistant Utils.llb/IVA Store Object Tracking Results.vi"/>
+				<Item Name="NI_Vision_Acquisition_Software.lvlib" Type="Library" URL="/&lt;vilib&gt;/vision/driver/NI_Vision_Acquisition_Software.lvlib"/>
+				<Item Name="NI_Vision_Development_Module.lvlib" Type="Library" URL="/&lt;vilib&gt;/vision/NI_Vision_Development_Module.lvlib"/>
+				<Item Name="Vision Info Type2.ctl" Type="VI" URL="/&lt;vilib&gt;/vision/Image Controls.llb/Vision Info Type2.ctl"/>
+			</Item>
+			<Item Name="niimaqdx.dll" Type="Document" URL="niimaqdx.dll">
+				<Property Name="NI.PreserveRelativePath" Type="Bool">true</Property>
+			</Item>
+			<Item Name="nivision.dll" Type="Document" URL="nivision.dll">
+				<Property Name="NI.PreserveRelativePath" Type="Bool">true</Property>
+			</Item>
+			<Item Name="nivissvc.dll" Type="Document" URL="nivissvc.dll">
+				<Property Name="NI.PreserveRelativePath" Type="Bool">true</Property>
+			</Item>
+		</Item>
 		<Item Name="Build Specifications" Type="Build"/>
 	</Item>
 </Project>
